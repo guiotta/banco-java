802b653237f03ab24e698b03af9b12a5ec960e41
Guilherme <guilhermeotta@gmail.com>
2020-02-04 22:03:18 -0300

    Adiciona lógica para gerar um número dec conta único.

--- src/main/java/br/com/otta/bank/account/service/AccountService.java
+++ src/main/java/br/com/otta/bank/account/service/AccountService.java
@@ -8,7 +8,7 @@ import br.com.otta.bank.account.builder.AccountBuilder;
 import br.com.otta.bank.account.configuration.AccountConfiguration;
 import br.com.otta.bank.account.entity.Account;
 import br.com.otta.bank.account.entity.AccountType;
-import br.com.otta.bank.account.factory.AccountNumberFactory;
+import br.com.otta.bank.account.factory.UniqueAccountNumberFactory;
 import br.com.otta.bank.account.mapper.AccountTypeMapper;
 import br.com.otta.bank.account.repository.AccountRepository;
 import br.com.otta.bank.client.entity.Client;
@@ -21,34 +21,41 @@ import br.com.otta.bank.client.entity.Client;
  */
 @Service
 public class AccountService {
+    private static final Object FLAG = new Object();
+
     /**
      * Esta dependência é configurável pelo arquivo de propriedades do projeto.
      */
     private final String agencyNumber;
     private final AccountRepository repository;
-    private final AccountNumberFactory accountNumberFactory;
+    private final UniqueAccountNumberFactory uniqueAccountNumberFactory;
     private final AccountTypeMapper accountTypeMapper;
 
     @Autowired
     public AccountService(@Qualifier(AccountConfiguration.AGENCY_BEAN_NAME) String agencyNumber,
-            AccountRepository repository, AccountNumberFactory accountNumberFactory,
+            AccountRepository repository, UniqueAccountNumberFactory uniqueAccountNumberFactory,
             AccountTypeMapper accountTypeMapper) {
+        super();
         this.agencyNumber = agencyNumber;
         this.repository = repository;
-        this.accountNumberFactory = accountNumberFactory;
+        this.uniqueAccountNumberFactory = uniqueAccountNumberFactory;
         this.accountTypeMapper = accountTypeMapper;
     }
 
     public void create(Client client) {
         AccountType type = accountTypeMapper.map(client.getType());
-        String accountNumber = accountNumberFactory.get();
-        Account account = new AccountBuilder()
-                .setNumber(accountNumber)
-                .setAgency(agencyNumber)
-                .setAccountType(type)
-                .setClient(client)
-                .build();
 
-        account = repository.save(account);
+        // Bloco sincronizado para garantir que o valor de accountNumber recém criado, ainda será unico ao salvar na base. 
+        synchronized (FLAG) {
+            String accountNumber = uniqueAccountNumberFactory.get();
+            Account account = new AccountBuilder()
+                    .setNumber(accountNumber)
+                    .setAgency(agencyNumber)
+                    .setAccountType(type)
+                    .setClient(client)
+                    .build();
+
+            account = repository.save(account);
+        }
     }
 }
