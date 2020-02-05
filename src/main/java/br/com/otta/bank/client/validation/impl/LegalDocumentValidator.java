5a0c7ca7a2e0c3e1ca9b205867314543e7f40ac0
Guilherme <guilhermeotta@gmail.com>
2020-02-05 17:52:46 -0300

    Adiciona validação de CPF e CNPJ.

--- src/main/java/br/com/otta/bank/client/validation/impl/LegalDocumentValidator.java
+++ src/main/java/br/com/otta/bank/client/validation/impl/LegalDocumentValidator.java
@@ -1,9 +1,12 @@
 package br.com.otta.bank.client.validation.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.otta.bank.client.validation.api.DocumentValidator;
 import br.com.otta.bank.client.validation.model.ValidationInformation;
+import br.com.safeguard.check.SafeguardCheck;
+import br.com.safeguard.types.ParametroTipo;
 
 /**
  * Componente para realizar a validação de um documento de pessoa juridica.</br>
@@ -16,6 +19,13 @@ import br.com.otta.bank.client.validation.model.ValidationInformation;
 public class LegalDocumentValidator implements DocumentValidator {
     private static final int EXPECTED_DOCUMENT_SIZE = 14;
 
+    private final SafeguardCheck safeguardCheck;
+
+    @Autowired
+    public LegalDocumentValidator(SafeguardCheck safeguardCheck) {
+        this.safeguardCheck = safeguardCheck;
+    }
+
     @Override
     public ValidationInformation validate(String document) {
         ValidationInformation validationInformation = new ValidationInformation();
@@ -25,6 +35,9 @@ public class LegalDocumentValidator implements DocumentValidator {
         }
         if (!this.validateOnlyDigits(document)) {
             validationInformation.addMessage("Invalid content for CNPJ. Expected only digits.");
+        }
+        if (safeguardCheck.elementOf(document, ParametroTipo.CNPJ).validate().hasError()) {
+            validationInformation.addMessage("Document is not a valid CNPJ.");
         }
 
         return validationInformation;
